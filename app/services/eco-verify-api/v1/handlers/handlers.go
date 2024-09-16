04bcf59439dc50a310a43d7734a3dd48ab37912d
Kunmeer-SyedMohamedHyder <syed-mohamed-hyder.kunmeer@hpe.com>
2024-09-16 19:55:26 +0530

    WebApp introduced to contain the shutdown channel

--- app/services/eco-verify-api/v1/handlers/handlers.go
+++ app/services/eco-verify-api/v1/handlers/handlers.go
@@ -3,12 +3,12 @@ package handlers
 import (
 	"github.com/SyedMohamedHyder/ecoVerify/app/services/eco-verify-api/v1/handlers/hackgrp"
 	v1 "github.com/SyedMohamedHyder/ecoVerify/business/web/v1"
-	"github.com/dimfeld/httptreemux/v5"
+	"github.com/SyedMohamedHyder/ecoVerify/foundation/web"
 )
 
 type Routes struct{}
 
 // Add implements the RouterAdder interface.
-func (Routes) Add(mux *httptreemux.ContextMux, cfg v1.APIMuxConfig) {
-	hackgrp.Routes(mux)
+func (Routes) Add(app *web.App, cfg v1.APIMuxConfig) {
+	hackgrp.Routes(app)
 }
